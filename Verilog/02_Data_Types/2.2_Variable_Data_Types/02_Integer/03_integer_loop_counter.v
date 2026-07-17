d0d28763c4e553e7aa3449dc738a2bad00a6d9c0
Saikumar <kalisetty.saikumar7s@gmail.com>
2026-07-17 21:46:18 +0530

    Add integer loop counter example

---
+++ Verilog/02_Data_Types/2.2_Variable_Data_Types/02_Integer/03_integer_loop_counter.v
@@ -0,0 +1,23 @@
+module integer_loop_counter;
+
+integer i;
+
+initial begin
+    for (i = 0; i < 8; i = i + 1) begin
+        $display("Iteration = %0d", i);
+    end
+
+    $finish;
+end
+
+endmodule
+
+/* Expected output
+Iteration = 0
+Iteration = 1
+Iteration = 2
+Iteration = 3
+Iteration = 4
+Iteration = 5
+Iteration = 6
+Iteration = 7 */
