e7b2ce457c4f35ce490d459315fce29a6425623d
Saikumar <kalisetty.saikumar7s@gmail.com>
2026-07-18 17:29:25 +0530

    Add integer signed behavior example

---
+++ Verilog/02_Data_Types/2.2_Variable_Data_Types/02_Integer/04_integer_signed_behavior.v
@@ -0,0 +1,28 @@
+module integer_signed_behavior;
+
+integer a;
+integer b;
+integer c;
+integer d;
+
+initial begin
+    a = -15;
+    b = 10;
+
+    c = a + b;
+    d = a - b;
+
+    $display("a = %0d", a);
+    $display("b = %0d", b);
+    $display("a + b = %0d", c);
+    $display("a - b = %0d", d);
+
+    if (a < b)
+        $display("a is less than b");
+    else
+        $display("a is greater than or equal to b");
+
+    $finish;
+end
+
+endmodule
